3a4e18fb1c4a921524753d1bdd90bde9c33ce78d
Jared Heeringa <jaredheeringa@icloud.com>
2023-12-07 22:21:15 -0600

    Initial Commit

---
+++ ToDo Application/ToDo_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ApplicationApp.swift
+//  ToDo Application
+//
+//  Created by Jared Heeringa on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
